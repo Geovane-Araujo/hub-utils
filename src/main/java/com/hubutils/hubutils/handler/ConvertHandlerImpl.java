2ea5112d6e96a6be37d5ea66883889e7156ff05d
Geovane Araújo <geovane-araujo@outlook.com>
2022-09-20 00:14:09 -0300

    certificado

--- src/main/java/com/hubutils/hubutils/handler/ConvertHandlerImpl.java
+++ src/main/java/com/hubutils/hubutils/handler/ConvertHandlerImpl.java
@@ -1,21 +1,20 @@
 package com.hubutils.hubutils.handler;
 
 import com.hubutils.hubutils.controller.ConvertController;
-import com.itextpdf.html2pdf.HtmlConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.Hashtable;
 
 @RestController
-@RequestMapping("utils")
-@CrossOrigin(origins = {"http:localhost:8090","*"})
+@RequestMapping("/utils")
 public class ConvertHandlerImpl {
 
     @Autowired
     ConvertController convertController;
 
     @PostMapping("/textImage")
+    @CrossOrigin(origins ="*")
     public ResponseEntity<?> textImage(@RequestBody Hashtable images){
         String ret = convertController.imageB64(images.get("imgString").toString(),images.get("imgText").toString());
         return ResponseEntity.ok().body(ret);
